d93ee6bb446b4ddad4cc6040a53706c2f50cd4ba
JEQS <jaime.quistian.21isc@tecsanpedro.edu.mx>
2025-03-14 14:05:04 -0600

    Creacion de modelo y creacion de clase para el context
    Creacion de modelo y creacion de clase para el context

---
+++ PracticeEntityFramework/Model/Location.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticeEntityFramework.Model
+{
+    public class Location
+    {
+        [Key]
+        public required short LocationId { get; set; }
+        public required string Name { get; set; }
+        public required decimal CostRate { get; set; }
+        public required decimal Availability { get; set; }
+        public required decimal ModifiedDate { get; set; }
+        public required DateTime ModifiedTime { get; set; }
+
+    }
+}
